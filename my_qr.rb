b2ae439bc4a6f89d060c554708ad14d23977a0f0
TaylorColon3 <stuwrkr-tcolon4@mx.uillinois.edu>
2023-02-20 20:15:51 +0000

    second wifi commit

--- my_qr.rb
+++ my_qr.rb
@@ -1,1 +1,9 @@
 p "hi"
+require "rqrcode"
+qrcode = RQRCode::QRCode.new("WIFI:T:WPA;S:TMOBILE-CECC;P:Eliamaris24;;")
+png = qrcode.as_png({ :size => 500 })
+IO.binwrite("sometext.png", png.to_s)
+
+qrcode = RQRCode::QRCode.new("WIFI:T:WPA;S:TMOBILE-CECC;P:Eliamaris24;;")
+
+# WIFI:T:WPA;S:TMOBILE-CECC;P:Eliamaris24;;
